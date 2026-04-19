96d2cedf5efec9d0eb7bf792bc8ed9260ce24b9e
Lê Thành Phát <141992022+phatleework7@users.noreply.github.com>
2026-04-19 08:38:37 +0700

    build on production

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+//1 TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // If i can i think can be don't hardcode 
